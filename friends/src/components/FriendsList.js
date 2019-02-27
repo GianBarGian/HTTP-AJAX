71b2fa5acfe8b1dceea7b4991722fb543a21b004
Giacomo Benati <giacomobenati@mailbox.org>
2019-02-27 14:20:23 +0100

    added updater functions for state

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,2 +1,25 @@
 import React from 'react';
 import axios from 'axios';
+
+
+export default class FriendsList extends React.Component {
+    state = {
+        friends: null,
+        error: null,
+        loading: false,
+    }
+
+    setFriends = friends => {
+        this.setState({ friends });
+    }
+
+    setError = error => {
+        this.setState({ error });
+    }
+
+    resetError = () => this.setState({ error: null })
+
+    startSpinner = () => this.setState({ loading: true })
+
+    stopSpinner = () => this.setState({ loading: false }) 
+}
