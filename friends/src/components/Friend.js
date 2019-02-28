50c24e6c8ca03981bab93c0fb8957c12d0cc3ceb
Giacomo Benati <giacomobenati@mailbox.org>
2019-02-28 14:53:38 +0100

    added delete button + form to add update a friend

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-export default function Friend ({ friend }) {
+export default function Friend ({ friend, deleteFriend }) {
     return (
         <div className="friend">
             <p>{friend.name}</p>   
             <p>{friend.age}</p>
             <p>{friend.email}</p>
+            <input onClick={() => deleteFriend(friend.id)} type="button" value="Delete" />
         </div>
     )
 }
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -8,7 +8,12 @@ export default class FriendsList extends React.Component {
         friends: [],
         error: null,
         loading: false,
+        selectedFriend: null,
     }
+
+    inputNameRef = React.createRef();
+    inputAgeRef = React.createRef();
+    inputEmailRef = React.createRef();
 
     componentDidMount() {
         this.fetchData('http://localhost:5000/friends')
@@ -21,7 +26,43 @@ export default class FriendsList extends React.Component {
         axios.get(url)
             .then(res => this.setFriends(res.data))
             .catch(this.setError)
-            .then(this.stopSpinner);
+            .finally(this.stopSpinner);
+    }
+
+    postFriend = () => {
+        const name= this.inputNameRef.current.value;
+        const age= this.inputAgeRef.current.value;
+        const email = this.inputEmailRef.current.value;
+        const id = this.state.friends.length + 1;
+
+        this.resetError();
+        this.startSpinner();
+        axios.post('http://localhost:5000/friends', { id, name, age, email})
+            .then(res => this.setFriends(res.data))
+            .catch(this.setError)
+            .finally(this.stopSpinner);
+    }
+
+    updateFriend = id => {
+        const name= this.inputNameRef.current.value;
+        const age= this.inputAgeRef.current.value;
+        const email = this.inputEmailRef.current.value;
+
+        this.resetError();
+        this.startSpinner();
+        axios.put(`http://localhost:5000/friends/${id}`, { name, age, email})
+            .then(res => this.setFriends(res.data))
+            .catch(this.setError)
+            .finally(this.stopSpinner);
+    }
+
+    deleteFriend = id => {
+        this.resetError();
+        this.startSpinner();
+        axios.delete(`http://localhost:5000/friends/${id}`)
+            .then(res => this.setFriends(res.data))
+            .catch(this.setError)
+            .finally(this.stopSpinner);
     }
 
     setFriends = friends => {
@@ -55,9 +96,28 @@ export default class FriendsList extends React.Component {
             <div>
                {
                    this.state.friends.map(friend => 
-                       <Friend friend={friend} />
+                        <Friend 
+                            key={friend.id} 
+                            deleteFriend={this.deleteFriend} 
+                            friend={friend} 
+                        />
                    )
                }
+                <form>
+                    <select name="friends" value={this.state.selectedFriend} onChange={e => this.setState({ selectedFriend: e.target.value })}>
+                        <option>Choose a friend</option>
+                        {
+                            this.state.friends.map(friend => 
+                                <option key={friend.id} value={friend.id}>{friend.name}</option>    
+                            )
+                        }
+                    </select>
+                    <input type="text" placeholder="name" ref={this.inputNameRef}/>
+                    <input type="number" placeholder="age" ref={this.inputAgeRef}/>
+                    <input type="email" placeholder="email" ref={this.inputEmailRef}/>
+                    <input onClick={this.postFriend} type="button" value="Submit"/>
+                    <input onClick={() => this.updateFriend(this.state.selectedFriend)} type="button" value="Update"/>
+                </form>
             </div>
         )
     }
